6ffc3f357d3dd85446ec58ce85525e6dc8b3f42f
Stricted <info@nexus-irc.de>
2013-04-26 22:35:56 +0200

    little bugfix #5

--- cmd/bind.cmd
+++ cmd/bind.cmd
@@ -46,30 +46,11 @@ if ($saxs >= 800) {
 	fclose($fope);
 	$found = 0;
 	$pazz = explode(".",$paz[1]);
-	if (file_exists("cmd/".$pazz[0].".cmd")) {
-		$fope = fopen($pazz[0].".cmd","r+");
+	if (file_exists("./cmd/".$pazz[0].".cmd")) {
+		$fope = fopen("./cmd/".$pazz[0].".cmd","r+");
 		while ($frae = fgets($fope)) {
-			$frae = str_replace("\r","",$frae);
-			$frae = str_replace("\n","",$frae);
-			$frge = explode(" ",$frae);
-			if ($frae{0} == "/") {
-				if (substr($params,strlen("$paz[0] $paz[1] ")) != "") {
-					$mgs = substr($params,strlen("$paz[0] $paz[1] "))." ";
-				}
-				else {
-					$mgs = "";
-				}
-				if (strtolower($frge[2]) == strtolower($pazz[1])) {
-					$fcont .= $paz[0]." mod_mod ".$pazz[0]." ".$pazz[1]." ".$mgs;
-					$found = 1;
-				}
-			}
-		}
-		fclose($fope);
-		if ($found == 0) {
-			sendserv("NOTICE $nick :\002".$pazz[1]."\002 is not a valid command in module ".$pazz[0].".");
-		}
-		else {
+			$mgs = substr($params,strlen("$paz[0] $paz[1] "))." ";
+			$fcont .= $paz[0]." mod_mod ".$pazz[0].$pazz[1]." ".$mgs;
 			$fope = fopen("./conf/bind.conf","w+");
 			fwrite($fope,$fcont);
 			fclose($fope);
@@ -77,25 +58,6 @@ if ($saxs >= 800) {
 		}
 	}
 	else {
-		if ($pazz[0] == "bot_internal") {
-			if (is_callable("bot_".$pazz[1],false,$callable_name)) {
-					if (substr($params,strlen("$paz[0] $paz[1] ")) != "") {
-				$mgs = substr($params,strlen("$paz[0] $paz[1] "))." ";
-				}
-				else {
-					$mgs = "";
-				}
-				$fcont .= $paz[0]." ".$pazz[1]." ".$mgs;
-				$fope = fopen("./conf/bind.conf","w+");
-				fwrite($fope,$fcont);
-				fclose($fope);
-				sendserv("NOTICE $nick :New command \002$paz[0]\002 bound to the bot.");     
-			}
-			else {
-				sendserv("NOTICE $nick :Command not found bot-internally.");
-			}
-			return(1);
-		}
 		sendserv("NOTICE $nick :There is no module with name \002$pazz[0]\002.");
 	}
 }
--- cmd/command.cmd
+++ cmd/command.cmd
@@ -17,33 +17,37 @@
 $params = $paramzz;
 $found = 0;
 $bip = explode(" ",$params);
-$fop = fopen("./conf/bind.conf","r+");
-while ($fra = fgets($fop)) {
-	$fra = str_replace("\r","",$fra);
-	$fra = str_replace("\n","",$fra);
-	$fgr = explode(" ",$fra);
-	if (strtolower($fgr[0]) == strtolower($bip[0])) {
-		if (strtolower($fgr[1]) == "mod_mod") {
-			$comothis = $fgr[2].".".$fgr[3];
-			$cmdthis = $fgr[2].".".substr($fra,strlen($fgr[0]." ".$fgr[1]." ".$fgr[2]." "));
-			sendserv("NOTICE $nick :\002$bip[0]\002 is a binding for: $cmdthis");
-			if (is_file("./defs/def._".strtolower($comothis)."_.txt")) {
-				sendserv("NOTICE $nick :\002".strtoupper($comothis)."\002");
-				$xx = explode("\n",str_replace("\r","",file_get_contents("./defs/def._".strtolower($comothis)."_.txt")));
-				foreach ($xx as $xy) {
-					cmd_parser($nick,$ident,$host,"help",$target,$target,$xy);
+if ($bip[0] != "") {
+	$fop = fopen("./conf/bind.conf","r+");
+	while ($fra = fgets($fop)) {
+		$fra = str_replace("\r","",$fra);
+		$fra = str_replace("\n","",$fra);
+		$fgr = explode(" ",$fra);
+		if (strtolower($fgr[0]) == strtolower($bip[0])) {
+			if (strtolower($fgr[1]) == "mod_mod") {
+				$comothis = $fgr[2].".".$fgr[3];
+				$cmdthis = $fgr[2]." ".substr($fra,strlen($fgr[0]." ".$fgr[1]." ".$fgr[2]." "));
+				sendserv("NOTICE $nick :\002$bip[0]\002 is a binding for: $cmdthis");
+				if (is_file("./defs/def._".strtolower($comothis)."_.txt")) {
+					sendserv("NOTICE $nick :\002".strtoupper($comothis)."\002");
+					$xx = explode("\n",str_replace("\r","",file_get_contents("./defs/def._".strtolower($comothis)."_.txt")));
+					foreach ($xx as $xy) {
+						cmd_parser($nick,$ident,$host,"help",$target,$target,$xy);
+					}
 				}
+				sendserv("NOTICE $nick :End of requirements for $bip[0].");
 			}
-			sendserv("NOTICE $nick :End of requirements for $bip[0].");
+			else {
+				sendserv("NOTICE $nick :\002$bip[0]\002 is a binding for bot_internal.".substr($fra,strlen($fgr[0]." ")));
+				sendserv("NOTICE $nick :End of requirements for $bip[0].");
+			}
+			$found = 1;
 		}
-		else {
-			sendserv("NOTICE $nick :\002$bip[0]\002 is a binding for bot_internal.".substr($fra,strlen($fgr[0]." ")));
-			sendserv("NOTICE $nick :End of requirements for $bip[0].");
-		}
-		$found = 1;
 	}
-}
-fclose($fop);
-if ($found == 0) {
-	sendserv("NOTICE $nick :\002$bip[0]\002 is an unknown command");
+	fclose($fop);
+	if ($found == 0) {
+		sendserv("NOTICE $nick :\002$bip[0]\002 is an unknown command");
+	}
+} else {
+	sendserv("NOTICE $nick :\002command\002 requires more parameters.");
 }
