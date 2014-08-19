f61a395e9a017b4ecec8e04a5b36e01afdb17186
synthtech <synthtech@outlook.com>
2014-08-19 14:52:07 -0400

    Fix author not showing up in webinfo

--- cmd/webinfo.cmd
+++ cmd/webinfo.cmd
@@ -18,6 +18,6 @@ require_once("./inc/Feed.class.php");
 $feed = new FeedClass;
 $data = $feed->parseFeed("https://nexus-irc.de/news/NewsFeed");
 foreach($data as $v) {
-	sendserv("NOTICE $nick :".$v->title." By ".$v->author." (".$v->pubDate.")");
+	sendserv("NOTICE $nick :".$v->title." by ".$v->children("dc",true)->creator." (".$v->pubDate.")");
 }
 sendserv("NOTICE $nick :The latest news can be found at https://nexus-irc.de/news/NewsOverview");
