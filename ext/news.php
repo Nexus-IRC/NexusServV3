7e6566a30f3400359ecef80be7cefade50f0ec87
synthtech <synthtech@outlook.com>
2015-02-26 22:49:41 -0500

    Add more feeds for news script

--- ext/news.php
+++ ext/news.php
@@ -16,46 +16,89 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>. 
  */
 $param = explode(" ",$params);
+$p = preg_replace("/\s+/", "", strtolower($params));
 require_once("./inc/Feed.class.php");
 $feed = new FeedClass;
 
-if (($param[0] == "") || ($param[0] == "world")) {
+if ($p == "" || $p == "reuters") {
 	$data = $feed->parseFeed("http://feeds.reuters.com/Reuters/worldNews");
 	for ($i=0;$i<5;$i++) {
 		echo "NOTICE $nick :".$data[$i]->title."\n";
 	}
 }
-elseif ($param[0] == "onion") {
+elseif ($p == "onion" || $p == "theonion") {
 	$data = $feed->parseFeed("http://feeds.theonion.com/theonion/daily");
 	for ($i=0;$i<5;$i++) {
 		echo "NOTICE $nick :".$data[$i]->title."\n";
 	}
 }
-elseif (($param[0] == "spiegel") || ($param[0] == "spiegel.de")) {
-	$data = $feed->parseFeed("http://www.spiegel.de/schlagzeilen/tops/index.rss");
-	for ($i=0;$i<5;$i++) {
-		echo "NOTICE $nick :".$data[$i]->title."\n";
-	}
-}
-elseif (($param[0] == "spiegel.en") || ($param[0] == "spiegel.int")) {
+elseif ($p == "spiegel") {
 	$data = $feed->parseFeed("http://www.spiegel.de/international/index.rss");
 	for ($i=0;$i<5;$i++) {
 		echo "NOTICE $nick :".$data[$i]->title."\n";
 	}
 }
-elseif (($param[0] == "tech") || ($param[0] == "arstechnica")) {
+elseif ($p == "arstechnica") {
 	$data = $feed->parseFeed("http://feeds.arstechnica.com/arstechnica/index");
 	for ($i=0;$i<5;$i++) {
 		echo "NOTICE $nick :".$data[$i]->title."\n";
 	}
 }
-elseif ($param[0] == "rt") {
+elseif ($p == "rt" || $p == "russiatoday") {
 	$data = $feed->parseFeed("http://rt.com/rss/");
 	for ($i=0;$i<5;$i++) {
 		echo "NOTICE $nick :".$data[$i]->title."\n";
 	}
 }
+elseif ($p == "readwrite") {
+	$data = $feed->parseFeed("http://readwrite.com/rss.xml");
+	for ($i=0;$i<5;$i++) {
+		echo "NOTICE $nick :".$data[$i]->title."\n";
+	}
+}
+elseif ($p == "theguardian") {
+	$data = $feed->parseFeed("http://www.theguardian.com/world/rss");
+	for ($i=0;$i<5;$i++) {
+		echo "NOTICE $nick :".$data[$i]->title."\n";
+	}
+}
+elseif ($p == "aljazeera") {
+	$data = $feed->parseFeed("http://www.aljazeera.com/xml/rss/all.xml");
+	for ($i=0;$i<5;$i++) {
+		echo "NOTICE $nick :".$data[$i]->title."\n";
+	}
+}
+elseif ($p == "githubnewfeatures") {
+	$data = $feed->parseFeed("https://github.com/blog/ship.atom");
+	for ($i=0;$i<5;$i++) {
+		echo "NOTICE $nick :".$data[$i]->title."\n";
+	}
+}
+elseif ($p == "githubengineering") {
+	$data = $feed->parseFeed("https://github.com/blog/engineering.atom");
+	for ($i=0;$i<5;$i++) {
+		echo "NOTICE $nick :".$data[$i]->title."\n";
+	}
+}
+elseif ($p == "appledeveloper") {
+	$data = $feed->parseFeed("https://developer.apple.com/news/rss/news.rss");
+	for ($i=0;$i<5;$i++) {
+		echo "NOTICE $nick :".$data[$i]->title."\n";
+	}
+}
+elseif ($p == "applepressrelease" || $p == "applepr") {
+	$data = $feed->parseFeed("https://www.apple.com/pr/feeds/pr.rss");
+	for ($i=0;$i<5;$i++) {
+		echo "NOTICE $nick :".$data[$i]->title."\n";
+	}
+}
+elseif ($p == "linuxkernel") {
+	$data = $feed->parseFeed("https://www.kernel.org/feeds/kdist.xml");
+	for ($i=0;$i<5;$i++) {
+		echo "NOTICE $nick :".$data[$i]->title."\n";
+	}
+}
 else {
-	echo "NOTICE $nick :".$param[0]." is not a valid news source.\n";
+	echo "NOTICE $nick :\002".$params."\002 is not a valid news source.\n";
 }
 ?>
